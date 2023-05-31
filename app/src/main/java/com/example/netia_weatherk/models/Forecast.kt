8689353a71c08ed832c03072e26eaf130f6a1c55
Netiar <collimow54@gmail.com>
2023-05-31 17:20:25 +0300

    add models package file

---
+++ app/src/main/java/com/example/netia_weatherk/models/Forecast.kt
@@ -0,0 +1,8 @@
+package com.example.netia_weatherk.models
+
+data class Forecast(
+    val name: String,
+    val temperature: Int,
+    val temperatureUnit: String,
+    val icon: String
+)
---
+++ app/src/main/java/com/example/netia_weatherk/models/WeatherResponse.kt
@@ -0,0 +1,9 @@
+package com.example.netia_weatherk.models
+
+class WeatherResponse {
+    val properties: Properties? = null
+
+    class Properties {
+        val periods: List<Forecast>? = null
+    }
+}
